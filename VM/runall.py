e7daa6c8108cfaeb6bf4857de3d7faa08012956a
inafi <irfan.k.nafi@gmail.com>
2020-04-15 13:35:04 -0400

    text

--- VM/runall.py
+++ VM/runall.py
@@ -106,7 +106,52 @@ def mrcnn():
         prev = init
 
 def text():
-    os.system("python3 exet.py")
+    init = 0
+    prev = 0
+
+    while(True):
+        init = int(cli.get('read').decode('utf-8'))
+        s = time.time()
+        if init != prev:
+            with open("/home/ec2-user/send.png", "rb") as image:
+                f = image.read() 
+                b = bytearray(f)
+                response = client.detect_document_text(
+                Document={
+                    'Bytes': b
+                }
+                )
+                columns = []
+                lines = []
+                for item in response["Blocks"]:
+                    if item["BlockType"] == "LINE":
+                        column_found=False
+                        for index, column in enumerate(columns):
+                            bbox_left = item["Geometry"]["BoundingBox"]["Left"]
+                            bbox_right = item["Geometry"]["BoundingBox"]["Left"] + item["Geometry"]["BoundingBox"]["Width"]
+                            bbox_centre = item["Geometry"]["BoundingBox"]["Left"] + item["Geometry"]["BoundingBox"]["Width"]/2
+                            column_centre = column['left'] + column['right']/2
+
+                            if (bbox_centre > column['left'] and bbox_centre < column['right']) or (column_centre > bbox_left and column_centre < bbox_right):
+                                lines.append([index, item["Text"]])
+                                column_found=True
+                                break
+                        if not column_found:
+                            columns.append({'left':item["Geometry"]["BoundingBox"]["Left"], 'right':item["Geometry"]["BoundingBox"]["Left"] + item["Geometry"]["BoundingBox"]["Width"]})
+                            lines.append([len(columns)-1, item["Text"]])
+
+                lines.sort(key=lambda x: x[0])
+                text = ""
+                for line in lines:
+                    text = text+line[1]
+                    text = text + '\n'
+                text = text.strip()
+
+            print(text)
+            ref = db.reference()
+            ref.update({"text":text})
+            print("text time:", time.time() - s, "\n")
+        prev = init
 
 def search():
     os.system("python3 exes.py")
